606cb13ab8e357523443ca1d2a9a4d1444be7341
jed <jeddatgonte2@gmail.com>
2025-12-31 21:28:01 +0800

    update main.py , allow_credentials to true

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,11 +11,10 @@ app.add_middleware(
     allow_origins=[
         "https://dqtool.vercel.app",
         "https://dqtoolbackend.vercel.app",
-        "http://localhost:3000",
         "http://127.0.0.1:8000",
         "http://127.0.0.1:3000",
     ],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
